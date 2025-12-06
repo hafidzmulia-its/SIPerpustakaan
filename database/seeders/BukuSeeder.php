f0ba27c8eea2e3864d5c4dcbd5f53375d6d5870b
hafidzmulia-its <5002221022@student.its.ac.id>
2025-12-06 16:37:54 +0700

    improve buku seeder

--- database/seeders/BukuSeeder.php
+++ database/seeders/BukuSeeder.php
@@ -15,25 +15,90 @@ class BukuSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+public function run(): void
     {
-        $faker = Faker::create();   
-        for( $i = 1; $i <= 10; $i++) {
-            Buku::create([
-                'kode_buku' => 'BKU' . str_pad($i, 3, '0', STR_PAD_LEFT), // Generate kode_buku like BKU001, BKU002, etc.
-                'judul' => $faker->sentence(3), // Example book title
-                'pengarang' => $faker->name(), // Example author name
-                'tahun_terbit' => 2000 + $i, // Example year of publication
-            ]);
+        $books = [
+            [
+                'kode_buku' => 'BKU000',
+                'judul' => 'Bumi Manusia',
+                'cover' => 'img/bumi_manusia.jpg',
+                'pengarang' => 'Pramoedya Ananta Toer',
+                'tahun_terbit' => 1980,
+            ],
+            [
+                'kode_buku' => 'BKU001',
+                'judul' => 'Laskar Pelangi',
+                'cover' => 'img/laskar_pelangi.jpg',
+                'pengarang' => 'Andrea Hirata',
+                'tahun_terbit' => 2005,
+            ],
+            [
+                'kode_buku' => 'BKU002',
+                'judul' => 'Laut Bercerita',
+                'cover' => 'img/laut_bercerita.jpg',
+                'pengarang' => 'Leila S. Chudori',
+                'tahun_terbit' => 2017,
+            ],
+            [
+                'kode_buku' => 'BKU003',
+                'judul' => 'Bumi',
+                'cover' => 'img/buimi.jpg',
+                'pengarang' => 'Tere Liye',
+                'tahun_terbit' => 2014,
+            ],
+            [
+                'kode_buku' => 'BKU004',
+                'judul' => 'Bumi',
+                'cover' => 'img/bumi.jpg',
+                'pengarang' => 'Tere Liye',
+                'tahun_terbit' => 2014,
+            ],
+            [
+                'kode_buku' => 'BKU005',
+                'judul' => 'Laut Bercerita',
+                'cover' => 'img/laut_bercerita.jpg',
+                'pengarang' => 'Leila S. Chudori',
+                'tahun_terbit' => 2017,
+            ],
+            [
+                'kode_buku' => 'BKU006',
+                'judul' => 'Matahari Minor',
+                'cover' => 'img/matahari_minor.jpg',
+                'pengarang' => 'Tere Liye',
+                'tahun_terbit' => 2015,
+            ],
+            [
+                'kode_buku' => 'BKU007',
+                'judul' => 'Perahu Kertas',
+                'cover' => 'img/perahu_kertas.jpg',
+                'pengarang' => 'Dee Lestari',
+                'tahun_terbit' => 2009,
+            ],
+            [
+                'kode_buku' => 'BKU008',
+                'judul' => 'Negeri 5 Menara',
+                'cover' => 'img/negeri_5_menara.jpg',
+                'pengarang' => 'Ahmad Fuadi',
+                'tahun_terbit' => 2009,
+            ],
+            [
+                'kode_buku' => 'BKU009',
+                'judul' => 'Bulan',
+                'cover' => 'img/bulan.jpg',
+                'pengarang' => 'Tere Liye',
+                'tahun_terbit' => 2015,
+            ],
+            [
+                'kode_buku' => 'BKU010',
+                'judul' => 'Bulan',
+                'cover' => 'img/bulan.jpg',
+                'pengarang' => 'Tere Liye',
+                'tahun_terbit' => 2015,
+            ],
+        ];
+
+        foreach ($books as $book) {
+            Buku::create($book);
         }
-        // Create a specific book with a known kode_buku
-        Buku::create([
-            'kode_buku' => 'BKU000',
-            'judul' => 'Buku Contoh',
-            'pengarang' => 'Pengarang Contoh',
-            'tahun_terbit' => 2020,
-            'cover' => 'img/no-cover.png', // Default cover image
-        ]);
-    
     }
 }
